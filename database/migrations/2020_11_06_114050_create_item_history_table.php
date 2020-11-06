dcdc1820c0fb75bc64ae527277b13a74ca14a39d
Ubuntu <ubuntu@ip-172-31-81-55.ec2.internal>
2020-11-06 02:54:31 +0000

    make migration file of item_history

---
+++ database/migrations/2020_11_06_114050_create_item_history_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemHistoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item_history', function (Blueprint $table) {
+            
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->unsignedBigInteger('user_id');; // 使用したユーザーid
+            $table->unsignedBigInteger('item_id');; // 使用した物品id
+            $table->integer('amount'); // 使用した個数
+            
+            // 外部キー制約
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null'); // usersが削除されたときに、userIdがnull
+            $table->foreign('item_id')->references('id')->on('items')->onDelete('set null'); // itemsが削除されたときに、itemIdがnull
+
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item_history');
+    }
+}
